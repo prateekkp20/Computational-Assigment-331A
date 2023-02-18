fa2d2f0d1d368f977af65544a29f8689332c9c99
prateekkp20 <prateekkp20@iitk.ac.in>
2023-02-18 12:14:59 +0530

    Some changes

--- Code.m
+++ Code.m
@@ -1,93 +1,70 @@
 % Define the constant values k1 and K2
-k1 = input('Enter k1\n');
-K2 = input('Enter k2\n');
+k1 = input('Enter k1: ');
+K2 = input('Enter k2: ');
 
 % Define the function f(Ca)
 syms Ca
 f(Ca) = (k1*Ca)/(1 + K2*Ca^2);
 
 % Define the domain of the function
-Ca_min = input('Enter Ca_min\n');
-Ca_max = input('Enter Ca_max\n');
+Ca_min = input('Enter Ca_min: ');
+Ca_max = input('Enter Ca_max: ');
 
 % (a) Derivative at a point
-Ca_point = 5;
+disp('Calculating derivative at a point')
+Ca_point = input('Enter the point: ');
 df_dCa = diff(f);
 derivative=vpa(subs(df_dCa,Ca,Ca_point));
+fprintf('The derivative at %d is %s\n',Ca_point,derivative);
 
 % (b) Area under the curve
-C = linspace(Ca_min, Ca_max);
-AUC = trapz(C, f(C));
+C = linspace(Ca_min,Ca_max);
+Area = trapz(C, f(C));
+fprintf('The area under the curve is: %s\n',vpa(Area))
 plot(C,f(C));
+hold on;
 
 % (c) Finding minimum or maximum functional value
-[minimum, minimum_index] = min(f(Ca));
-[maximum, maximum_index] = max(f(Ca));
+[minimum, minimum_index] = min(f(C));
+[maximum, maximum_index] = max(f(C));
+fprintf('The minimum value in the given domain is %s \n',vpa(minimum));
+fprintf('The maximum value in the given domain is %s \n',vpa(maximum));
 
 % (d) Drawing a straight line between two points on the function
-Ca1 = 3;
-Ca2 = 7;
+disp('Drawing a straight line between two points on the function');
+Ca1 = input('Enter the first point: ');
+Ca2 = input('Enter the second point: ');
 y1 = f(Ca1);
 y2 = f(Ca2);
 slope = (y2 - y1)/(Ca2 - Ca1);
 intercept = y1 - slope * Ca1;
 line = @(x) slope * x + intercept;
+plot([vpa(Ca1) vpa(Ca2)],[vpa(y1) vpa(y2)]);
+fprintf('Slope of the line is %s \n',vpa(slope));
+fprintf('Intercept on the Y-axis is %s \n',vpa(intercept));
 
 % (e) Searching for a point where the tangent is the same as the given slope
 
-% format longg
-% f1 = f;
-% f2_prime = df_dCa;
-% x1 = input('Enter starting point\n');
-slope_target=input('Enter the slope Target\n');
-% disp('Now subsequent enter stopping criteria:');
-% rel_err = input('Enter the relative error allowed in solution\n');
-% % func_value = input('Enter Convergence criteria for the function value, i.e., how close f(x) is to zero\n');
-% max_iter = input('Enter allowed maximum number of iterations\n');
+% slope_target=input('Enter the slope Target: \n');
+% Ca_tangent = fzero(@(x) (diff(f) - slope_target), [Ca_min,Ca_max]);
+% Evaluate the function at the interval endpoints
+f_min = double(f(Ca_min));
+f_max = double(f(Ca_max));
 
-% x2 = x1 - (f1(x1)-slope_target*x1)/f2_prime(x1);
-% err = 100;
-% iter = 0; 
-% stop_err = 0;
-% stop_iter=0;
-% stop_value=0;
-% i=1;
+% Check that the function values at the interval endpoints are finite and real
+if ~isfinite(f_min) || ~isreal(f_min) || ~isfinite(f_max) || ~isreal(f_max)
+error('Function values at interval endpoints must be finite and real.')
+end
 
-% while ((err > rel_err) && (iter<max_iter)) ...
-% %         && (abs(f1(x2))>func_value))
-%     x2 = x1 - f1(x1)/f2_prime(x1);
-%     err = abs(100*(x2-x1)/x2);
+% Prompt the user for the slope target
+slope_target=input('Enter the slope target: \n');
 
+% Find the tangent point Ca_tangent using fzero
+try
+Ca_tangent = fzero(@(x) (diff(f) - slope_target), [Ca_min,Ca_max]);
+catch
+error('Unable to find a tangent point. Check that the function and interval are valid.')
+end
 
-%     if(err<rel_err)
-%         stop_err=1;
-%     end
-
-%     if(abs(f1(x2))<func_value)
-%         stop_value=1;
-%     end
-%     x1 = x2;
-%     p=x2;
-%     errg(i) = err;
-%     iter = iter+1;
-%     i=i+1;
-%     if(iter==max_iter)
-%         stop_iter=1;
-%     end
-%     error(iter)=err;
-% end
-
-% fprintf('The required Value is %f\n',p);
-
-% if(stop_err)
-%     disp('Iterations stopped as relative error stopping criteria was met');
-% end
-
-% if(stop_iter)
-%     disp('Iterations stopped as maximum number of iterations were executed');
-% end
-
-% if(stop_value)
-%     disp('Iterations stopped as function value was close to zero as required');
-% end
-Ca_tangent = fzero(@(x) (diff(f) - slope_target), [Ca_min Ca_max]);
+% Display the tangent point
+fprintf('The tangent point is Ca = %f\n', Ca_tangent)
